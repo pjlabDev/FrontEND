38780dc7e5cb91366b45ae78e3b1bc8ec25cde1d
PedrassoxD <pedroperlab@gmail.com>
2020-06-04 19:01:54 +0200

    Deploy to Heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('/dist/pfpedro'));
+app.use(express.static('/dist/PFPedro'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('/dist/pfpedro/index.html'));
+    res.sendFile(path.join('/dist/PFPedro/index.html'));
 });
 app.listen(process.env.PORT || 8080);
